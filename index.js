d49e6a8d58020981dcd00001328cc4228e58cc32
Luane Salas <luanenicolesalaschoquevf3@gmail.com>
2021-11-16 18:26:21 -0500

    navbar

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar-fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
